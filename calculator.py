a4eb290109748420d5f239dc8ac644b22ebd6232
lorgu <15050522059@163.com>
2025-02-10 15:30:59 +0800

    fix divided by 0

--- calculator.py
+++ calculator.py
@@ -10,10 +10,13 @@ def divide(a, b):
         float: 除法运算的结果
         
     异常:
+        ZeroDivisionError: 当除数为0时抛出
         TypeError: 当输入参数不是数字类型时抛出
     """
     if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
         raise TypeError("输入必须是数字类型")
         
+    if b == 0:
+        raise ZeroDivisionError("除数不能为0")
         
     return a / b 
